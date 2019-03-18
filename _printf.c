d39580f4e1064e4e56d6684f53e9ae7b5722feea
dwkwan <dwkwan1989@gmail.com>
2019-03-18 21:01:43 +0000

    Remove % structure from array of structures. Add if and else if statements for when the % is followed by % or a non-specifer. Add comments sections for helper functions. Add betty style

--- _printf.c
+++ _printf.c
@@ -1,28 +1,39 @@
 #include "holberton.h"
 #include <stdarg.h>
+/**
+ * _printf - produces output according to format
+ * @format: format string containing zero or more directives
+ *
+ * Return: the number of characters printed, excluding null byte
+ */
 int _printf(const char *format, ...)
 {
-        data_t types[] = {
-                {"c", _printchar},
-                {"s", _printstring},
+	data_t types[] = {
+		{"c", _printchar},
+		{"s", _printstring},
 		{"i", _printint},
-                {"d", _printint},
-		{"%", _printchar},
+		{"d", _printint},
 		{NULL, NULL}
-        };
-	int i;
-	int j;
-	int counter = 0;
+	};
+	int i, j, counter = 0;
 	va_list ap;
 
 	va_start(ap, format);
-
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
+			counter += _putchar(format[i]);
+		else if (format[i + 1] == '%')
 		{
-			_putchar(format[i]);
-			counter++;
+			counter += _putchar('%');
+			i++;
+		}
+		else if (format[i + 1] != 'c' && format[i + 1] != 's'
+			 && format[i + 1] != 'i' && format[i + 1] != 'd')
+		{
+			counter += _putchar('%');
+			counter += _putchar(format[i + 1]);
+			i++;
 		}
 		else
 		{
@@ -35,32 +46,5 @@ int _printf(const char *format, ...)
 		}
 	}
 	va_end(ap);
-	printf("%i\n", counter);
 	return (counter);
 }
-
-/**
-	while (format[i] != '\0')
-	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			counter ++;
-		}
-		else
-		{
-			j = 0;
-			while (types[j].type != NULL)
-			{
-				if (format[i + 1] != '\0' &&
-				    format[i + 1] == *types[j].type)
-					counter += types[j].func(ap);
-				j++;
-			}
-		}
-		i++;
-	}
-	return counter;
-	va_end(ap);
-}
-*/
--- helper_funcs.c
+++ helper_funcs.c
@@ -10,17 +10,28 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
+/**
+ * _printchar - prints a character
+ * @ap: The character to print, argument pointer
+ *
+ * Return: 1
+ */
 int _printchar(va_list ap)
 {
 	_putchar(va_arg(ap, int));
 	return (1);
 }
-
+/**
+ * _printstring - prints a string
+ * @ap: The string to print, argument pointer
+ *
+ * Return: i
+ */
 int _printstring(va_list ap)
 {
 	char *s = va_arg(ap, char *);
 	int i = 0;
+
 	if (s == NULL)
 	s = "(null)";
 	while (s[i] != '\0')
@@ -30,7 +41,12 @@ int _printstring(va_list ap)
 	}
 	return (i);
 }
-
+/**
+ * _printint - prints an integer
+ * @ap: The string to print, argument pointer
+ *
+ * Return: counter
+ */
 int _printint(va_list ap)
 {
 	 int n = va_arg(ap, int);
@@ -49,7 +65,7 @@ int _printint(va_list ap)
 		answer = n / divisor;
 		divisor = divisor / 10;
 	}
-	while(divisor >= 1)
+	while (divisor >= 1)
 	{
 		counter += _putchar(answer);
 		answer = u / divisor;
